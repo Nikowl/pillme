15bdf8879fbaf69b08306c7c9e35e801ad7deb6c
Nikowl <fil.nikolya@gmail.com>
2021-05-21 16:49:10 +0300

    1. Создал функцию в файле /components/products/function.php  getBrands(PDO $Connection): array 2. Починил заплаткой  функцию getProducts. Не работал поиск по наименованию. (не может явно понять откуда взять столбец name. В условии WHERE алиас не работает. Времено изменил поле name в таблице products на productName)

--- components/products/function.php
+++ components/products/function.php
@@ -16,7 +16,7 @@
                 }
                 $prepareQuery[] = '(' . implode(' OR ', $values) . ')';
             } elseif (!empty($valueFilter)) {
-                $prepareQuery[] = $filter . " LIKE '%" . addslashes($valueFilter) . "%'";
+                $prepareQuery[] = '(' .$filter . " LIKE '%" . addslashes($valueFilter) . "%'" . ')';
             }
 
         }
@@ -25,7 +25,7 @@
             return null;
         }
         //TODO Добавить экранирование спец символов
-        return ' WHERE ' . implode(' AND ', $prepareQuery);
+        return ' WHERE ' . implode(' AND ', $prepareQuery) . ' ';
     }
 
     function getAmountPages(PDO $connection, array $filters = [], $limit = 12): int
@@ -47,8 +47,13 @@
         // TODO: по идее, эта функция должна возвращать массив товаров как есть.
         // TODO: ты тут подцепляешь бренды, чтобы красиво отобразить название бренда в списке
         // TODO: пока можно оставить так, так как у тебя мало где используется эта функция и всего 1 джоин
+        // TODO: не может явно понять откуда взять столбец name в условии WHERE алиас не работает. Времено изменил поле name в таблице products на productName
+        return $connection->query("SELECT p.*,  b.id as brandID, b.name as brandName FROM products as p INNER JOIN brands as b ON p.brandID = b.id $filtersQuery LIMIT $limit OFFSET $offset;")->fetchAll(PDO::FETCH_OBJ);
+    }
 
-        return $connection->query("SELECT p.*, b.id as brandID, b.name as brandName FROM products as p INNER JOIN brands as b ON p.brandID = b.id $filtersQuery LIMIT $limit OFFSET $offset;")->fetchAll(PDO::FETCH_OBJ);
+    function getBrands(PDO $connection): array
+    {
+        return $connection->query("SELECT id as brandID , name as brandName  FROM brands;")->fetchAll(PDO::FETCH_OBJ);
     }
 
     function normalizeProductsQuery(array $params): array
