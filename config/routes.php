61f3845ba75004a51f9e6dea320c3812220b0628
Nikowl <fil.nikolya@gmail.com>
2021-05-25 20:00:03 +0300

    1. Добавлен параметр methods для хранения http-метода. 2. В index.php добавлена обработка метода запроса.

--- config/routes.php
+++ config/routes.php
@@ -3,29 +3,30 @@
     return [
         "/" => [
             "handler" => false,
-            "file" => ROOT_DIR . "/components/products/index.php"
+            "file" => ROOT_DIR . "/components/products/index.php",
+            "methods" => ['GET']
         ],
         // Товары
         "/addProduct" => [
             "handler" => false,
-            "file" => ROOT_DIR . "/components/products/add.php"
+            "file" => ROOT_DIR . "/components/products/add.php",
+            "methods" => ['GET']
+
         ],
         // TODO: не должен открываться с методом GET (прочитать про ошибка 405)
         // TODO: сейчас обработка происходит в самом файле, а это работа роутера (обработка в index.php)
         //TODO: если не правильный метод, то бросать на 404
         "/createProduct" => [
-            "methods" => ['POST'],
             "handler" => true,
             "file" => ROOT_DIR . "/components/products/create.php",
+            "methods" => ['POST']
         ],
-        "/filters" => [
-            "handler" => true,
-            "file" => ROOT_DIR . "/components/product/function.php",
-        ],
+
 
         // Статические страницы
         "404" => [
             "handler" => false,
             "file" => ROOT_DIR . "/components/pages/404.php",
+            "methods" => ['GET']
         ]
     ];
--- index.php
+++ index.php
@@ -6,8 +6,12 @@
     // роут
     $routes = require_once(ROOT_DIR . '/config/routes.php');
     $requestedRoute = explode('?', $_SERVER['REQUEST_URI'])[0] ?? null;
+//    var_dump($requestedRoute);exit();
     $requestedMethod = $_SERVER['REQUEST_METHOD'];
     $route = $routes[$requestedRoute] ?? $routes['404'];
+    if (!in_array($requestedMethod,$route['methods'])) {
+        $route = $routes['404'];
+    }
     if ($route['handler']) {
         // обработчик
         require_once($route['file']);
